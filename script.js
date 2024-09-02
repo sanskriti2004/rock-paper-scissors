fc3074d671c83a8860c20c7894a0def33cc684d4
dizzy gogoi <sanskritigogoi2004@gmail.com>
2024-09-02 21:50:39 +0530

    experimented with html and js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("chicago");
+const btn = document.querySelector(".playBtn");
+const choice = document.querySelector(".choice");
+btn.addEventListener("click", () => {
+  if (
+    choice.value != "rock" ||
+    choice.value != "paper" ||
+    choice.value != "scissors"
+  ) {
+    alert("please choose rock, paper or scissors");
+  }
+});
