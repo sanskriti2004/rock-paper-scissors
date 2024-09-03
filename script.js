6c44ecd05ae1d96435f171e4fa7e6f916b71f8d3
dizzy gogoi <sanskritigogoi2004@gmail.com>
2024-09-03 19:35:35 +0530

    created the js logic for the game

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 const btn = document.querySelector(".playBtn");
 const choice = document.querySelector(".choice");
+
+const game = () => {
+  const computer = Math.ceil(Math.random() * 30);
+  if (computer < 10) {
+    return "rock";
+  } else if (computer < 20) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+};
+
 btn.addEventListener("click", () => {
   if (
-    choice.value != "rock" ||
-    choice.value != "paper" ||
-    choice.value != "scissors"
+    choice.value == "rock" ||
+    choice.value == "paper" ||
+    choice.value == "scissors"
   ) {
-    alert("please choose rock, paper or scissors");
+    const computerChoice = game();
+    const result = choice.value == computerChoice ? "Won" : "Lost";
+    alert(
+      "Your choice : " +
+        choice.value +
+        "\nComputer's choice : " +
+        computerChoice +
+        "\nYou " +
+        result +
+        "!!"
+    );
+  } else {
+    alert("Please choose from either rock, paper or scissors");
   }
 });
